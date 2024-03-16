21aa1011ae29e001c829b1cff31627260ae0a532
Hasan-TechNinja <hasantechninja@gmail.com>
2024-03-16 06:41:48 +0600

    create models and register amdin pannel

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import(
+    Manager,
+    Border,
+    MonthlyReport,
+    Meal,
+    Income,
+    Cost,
+    TransactionType,
+)
+
+@admin.register(Manager)
+class ManagerModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'user', 'name', 'phone', 'role', 'division', 'address', 'status', 'join_date', 'photo']
+
+@admin.register(Border)
+class BorderModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'user', 'name', 'phone', 'role', 'division', 'address', 'status', 'join_date', 'photo']
+
+@admin.register(MonthlyReport)
+class MonthlyReportModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'date', 'totalIncome', 'mealRate', 'totalBorders']
+
+@admin.register(Meal)
+class MealModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'status', 'amount', 'date', 'period']
+
+@admin.register(Income)
+class IncomeModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'user', 'type', 'amount', 'date']
+
+@admin.register(Cost)
+class CostModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'user', 'type', 'amount', 'date']
+
+@admin.register(TransactionType)
+class TransactionTypeModelAdmin(admin.ModelAdmin):
+    list_display=['id', 'title', 'description']
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+ROLE_CHOICES = (
+    ('Manageer', 'Manager'),
+    ('Border', 'Border')
+)
+STATUS_CHOICES = (
+    ('On', 'On'),
+    ('Off', 'Off')
+)
+DIVISION_CHOICES = (
+    ('Dhaka', 'Dhaka'),
+    ('Rangpur',  'Rangpur')
+    ('Rahshahi', 'Rajshahi'),
+    ('Khulna', 'Khulna'),
+    ('Barishal', 'Barishal'),
+    ('Chattogram', 'Chattogram'),
+    ('Mymensingh', 'Mymensingh'),
+    ('Sylhet', 'Sylhet')
+)
+class Manager(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone = models.IntegerField(max_length=20)
+    role = models.Choices(choices=ROLE_CHOICES, max_length=2)
+    division = models.Choices(choices=DIVISION_CHOICES)
+    address = models.CharField(max_length=50)
+    status = models.Choices(choices=STATUS_CHOICES, max_length=50)
+    join_date = models.DateField()
+    photo = models.ImageField(upload_to='Manager-Image')
+
+    def __str__(self):
+        return str(self.id)
+    
+class Border(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone = models.IntegerField(max_length=20)
+    role = models.Choices(choices=ROLE_CHOICES, max_length=2)
+    division = models.Choices(choices=DIVISION_CHOICES)
+    address = models.CharField(max_length=50)
+    status = models.Choices(choices=STATUS_CHOICES, max_length=50)
+    join_date = models.DateField()
+    photo = models.ImageField(upload_to='Border-Image') 
+    
+    def __str__(self):
+        return str(self.id)
+    
+class MonthlyReport(models.MOdel):
+    date = models.DateField()
+    totalIncome = models.IntegerField() 
+    totalCost = models.IntegerField()
+    mealRate = models.IntegerField()
+    totalBorders = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.id) 
+    
+
+PERIOD_CHOICES = (
+    ('Breakfast', 'Breakfast'),
+    ('Lunch', 'Lunch'),
+    ('Dinner', 'Dinner')
+)
+class Meal(models.Model):
+    status = models.Choices(choice=STATUS_CHOICES)
+    amount = models.IntegerField(max_length=10)
+    date = models.DateField()
+    period = models.Choices(choices=PERIOD_CHOICES)
+
+    def __str__(self):
+        return str(self.id)
+    
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    amount = models.IntegerField(max_length=20)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.id)
+    
+class Cost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    amount = models.IntegerField(max_length=20)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.id)
+
+class TransactionType(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return str(self.id)
